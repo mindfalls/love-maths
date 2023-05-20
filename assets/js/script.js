caf5b421d81238bb326adb3eb53ce576826bb2e6
Mattias Lundkvist <mindfalls@hotmail.com>
2023-05-20 10:36:05 +0000

    add code to generate two random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,34 @@
-function runGame() {
 
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+/**
+ * The main game "loop, called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame() {
+// Creates two random numbers between 1 and 25
+let num1 = Math.floor(Math.random() * 25) + 1;
+let num2 = Math.floor(Math.random() * 25) + 1;
 }
 function checkAnswer() {
 
 }
-function() {
-    calculateCorrectAnswer
+function calculateCorrectAnswer() {
+
 }
+    
 function incrementScore() {
 
 }
@@ -23,5 +45,6 @@ function displayMultiplyQuestion() {
 
 }
 function displayDivideQuestion() {
-    
+
 }
+console.log("HEllo")
